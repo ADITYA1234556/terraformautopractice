d804d11ba28b68896ad2647e785f3024f5e65832
aditya <aditya@gmail.com>
2024-10-25 13:57:34 +0100

    added bucket

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "111-aditya-kms"
     key            = "terraform/terraform.tfstate"
-    region         =  var.REGION["london"]
+    region         =  "eu-west-2"
   }
 }
